b689a18247822c6cb5a946ae009800904ddd6cfa
BamMaria <70711349+BamMaria@users.noreply.github.com>
2020-09-15 13:37:44 +0300

    Три экземпляра класса Point

--- OOP/src/main/java/Main.java
+++ OOP/src/main/java/Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main (String args[]){
-        Person Maria = new Person();
+        /*Person Maria = new Person();
         Person Pasha = new Person (2);
         Person Sergey = new Person("Сергей","Бамбуров",1);
         Person Elena = new Person ("Елена","Степаненко");
 
         Maria.setFrstName("Masha");
         Maria.setLastName("Bamburova");
-        Maria.setPassportId(2);
+        Maria.setPassportId(2);*/
+        Point tochka = new Point (4,5,2);
+        Point tochka2=new Point (10,5,20);
+        Point tochka3=new Point (15,20,17);
 
     }
-    
+
 
 }
