7dcf6a87d55843a994182aa7a0b944249aee64d5
BamMaria <70711349+BamMaria@users.noreply.github.com>
2020-09-15 13:35:31 +0300

    Конструктор Point
    Создан констрктор с тремя параметрами

--- OOP/src/main/java/Point.java
+++ OOP/src/main/java/Point.java
@@ -2,6 +2,12 @@ public class Point {
     private final double X;
     private final double Y;
     private final double Z;
+    public Point(double x, double y, double z) {
+        this.X =x;
+        this.Y=y;
+        this.Z=z;
+
+    }
 
 
 }
