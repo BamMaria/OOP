10ec77fac514f6c89e618e75aec8782420831829
BamMaria <70711349+BamMaria@users.noreply.github.com>
2020-09-15 11:36:52 +0300

    Экземпляр класса
    Создан экземпляр класса

--- OOP/src/main/java/Main.java
+++ OOP/src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main (String args[]){
-        System.out.println( " Hello world !!!");
+        Person Maria = new Person();
+        Maria.setFrstName("Masha");
+        Maria.setLastName("Bamburova");
+        Maria.setPassportId(2);
+
     }
 }
