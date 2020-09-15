6bb46fd080f9d4e8da6b57c1feb826fc4da037db
BamMaria <70711349+BamMaria@users.noreply.github.com>
2020-09-15 13:36:58 +0300

    Создание геттеров и сеттеров

--- OOP/src/main/java/Point.java
+++ OOP/src/main/java/Point.java
@@ -9,6 +9,30 @@ public class Point {
 
     }
 
+    public double getX() {
+        return X;
+    }
+
+    public double getY() {
+        return Y;
+    }
+
+    public double getZ() {
+        return Z;
+    }
+    public double setX() {
+        return this.X;
+    }
+
+    public double setY() {
+        return this.Y;
+    }
+
+    public double setZ() {
+        return this.Z;
+    }
+
+
 
 }
 
