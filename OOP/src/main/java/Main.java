55e289c3dfab09857d3168e9c7385dbffd4af415
BamMaria <70711349+BamMaria@users.noreply.github.com>
2020-09-15 12:40:51 +0300

    Создано три объекта
    Создано три объекта ,задействовавшие все конструкторы.

--- OOP/src/main/java/Main.java
+++ OOP/src/main/java/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main (String args[]){
         Person Maria = new Person();
+        Person Pasha = new Person (2);
+        Person Sergey = new Person("Сергей","Бамбуров",1);
+        Person Elena = new Person ("Елена","Степаненко");
+
         Maria.setFrstName("Masha");
         Maria.setLastName("Bamburova");
         Maria.setPassportId(2);
 
     }
+    
+
 }
