135028412e18a291c39ac97563ffa4b110e5a1ae
BamMaria <70711349+BamMaria@users.noreply.github.com>
2020-09-15 11:35:59 +0300

    Person
    Создан класс Person , добавлены геттеры и сеттеры

---
+++ OOP/src/main/java/Person.java
@@ -0,0 +1,31 @@
+public class Person {
+    private String frstName ;
+    private String lastName;
+    private int passportId ;
+
+    public String getFrstName() {
+        return frstName;
+    }
+
+    public void setFrstName(String frstName) {
+        this.frstName = frstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPassportId() {
+        return passportId;
+    }
+
+    public void setPassportId(int passportId) {
+        this.passportId = passportId;
+    }
+
+}
+
