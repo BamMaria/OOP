9fe2b836156bfac98fe3a3696320ba8fb1baeb93
BamMaria <70711349+BamMaria@users.noreply.github.com>
2020-09-18 15:35:54 +0300

    1.10main

--- OOP/src/main/java/Main.java
+++ OOP/src/main/java/Main.java
@@ -1,15 +1,15 @@
 
 public class Main {
     public static void main (String args[]){
-        /*Person Maria = new Person();
+        Person Maria = new Person();
         Person Pasha = new Person (2);
         Person Sergey = new Person("Сергей","Бамбуров",1);
         Person Elena = new Person ("Елена","Степаненко");
 
         Maria.setFrstName("Masha");
         Maria.setLastName("Bamburova");
-        Maria.setPassportId(2);*/
-        Point tochka = new Point (4,5,2);
+        Maria.setPassportId(2);
+       /* Point tochka = new Point (4,5,2);
         Point tochka2=new Point (10,5,20);
         Point tochka3=new Point (15,20,17);
         Point result = new Point(0,0,0);
@@ -20,9 +20,10 @@ public class Main {
         result=Points.enlarge(tochka,1);
         result=Points.opposite(tochka);
         result=Points.inverse(tochka);
+        result=Points.scalarProduct(tochka,tochka2);
 
 
-        System.out.println(tochka.length());
+        System.out.println(tochka.length());*/
 
 
 
