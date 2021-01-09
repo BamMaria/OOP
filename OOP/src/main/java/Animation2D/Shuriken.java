d3a8a50b7990082be17425d191055396264c683e
BamMaria <70711349+BamMaria@users.noreply.github.com>
2021-01-09 20:30:19 +0300

    Update Shuriken.java

--- OOP/src/main/java/Animation2D/Shuriken.java
+++ OOP/src/main/java/Animation2D/Shuriken.java
@@ -1,4 +1,5 @@
 package Animation2D;
+
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
@@ -9,13 +10,13 @@ import javax.imageio.ImageIO;
 
 public class Shuriken {
 
-    int x , y;
+    int x, y;
     int veloX;
     BufferedImage shuriken;
     Image[][] shurikenSprite;
-    int spriteState , lebar , tinggi;
+    int spriteState, lebar, tinggi;
 
-    public Shuriken(int x,int y,int veloX){
+    public Shuriken(int x, int y, int veloX) {
         this();
         this.x = x;
         this.y = y;
@@ -32,30 +33,30 @@ public class Shuriken {
         }
 
         shurikenSprite = new Image[1][10];
-        lebar = shuriken.getWidth()/10;
-        tinggi = shuriken.getHeight()/1;
+        lebar = shuriken.getWidth() / 10;
+        tinggi = shuriken.getHeight() / 1;
 
-        for(int i=0;i<2;i++){
-            shurikenSprite[0][i] = shuriken.getSubimage(lebar*i, tinggi*0, lebar, tinggi).getScaledInstance(50, 50, 0);
+        for (int i = 0; i < 2; i++) {
+            shurikenSprite[0][i] = shuriken.getSubimage(lebar * i, tinggi * 0, lebar, tinggi).getScaledInstance(50, 50, 0);
         }
 
     }
 
-    public void update(){
+    public void update() {
 
         x += veloX;
 
         spriteState++;
 
-        if(spriteState >= 2){
+        if (spriteState >= 2) {
             spriteState = 0;
         }
 
     }
 
-    public void render(Graphics g){
+    public void render(Graphics g) {
 
-        g.drawImage(shuriken, x,y ,null);
+        g.drawImage(shuriken, x, y, null);
 
     }
 
