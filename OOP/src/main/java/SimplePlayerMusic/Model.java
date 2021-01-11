2ef082d2f5a68e340d0eee76b48adc4dac3eef14
BamMaria <70711349+BamMaria@users.noreply.github.com>
2021-01-11 16:00:52 +0300

    Controller отделен от main

--- OOP/src/main/java/SimplePlayerMusic/Model.java
+++ OOP/src/main/java/SimplePlayerMusic/Model.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 
 public class Model {
     private ArrayList<String> playlist;
-
+    public void loadSongs() {
+        playlist.add("music/BnHA_op1.wav");
+        playlist.add("music/G_S2_ed3.wav");
+        playlist.add("music/G_S2_ed5.wav");
+        playlist.add("music/G_S2_op2.wav");
+        playlist.add("music/G_S2_op4.wav");
+    }
     Model() {
         playlist = new ArrayList<String>();
     }
@@ -19,5 +25,10 @@ public class Model {
 
     public int getCount() {
         return playlist.size();
+    }
+    public void printAll() {
+        for (String songName : playlist) {
+            System.out.println("Song name: " + songName);
+        }
     }
 }
