0fa106526bb98eadbfa4109941b0500b946397b6
BamMaria <70711349+BamMaria@users.noreply.github.com>
2021-01-13 15:02:47 +0300

    4.2

---
+++ OOP/src/test/java/part4/ExceptionsTest.java
@@ -0,0 +1,20 @@
+package part4;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ExceptionsTest {
+    ExceptionsClass exceptionsClass=new ExceptionsClass();
+
+    @Test
+    public void testStringMassif(){
+        assertEquals(exceptionsClass.StringMassif(new String[]{"Hello","World"},2),new char[]{'l','r'});
+        assertEquals(exceptionsClass.StringMassif(new String[]{"облако","йод","груша"},2),new char[]{'л','д','у'});
+
+        assertThrows(NullPointerException.class,()->{
+            exceptionsClass.StringMassif(new String[3],1);
+        });
+    }
+}
+
