7be370d42c60ae8f865f7e886c391d8af62f8e72
BamMaria <70711349+BamMaria@users.noreply.github.com>
2021-01-13 15:28:47 +0300

    4.5

---
+++ OOP/src/main/java/part4/Clone2.java
@@ -0,0 +1,8 @@
+package part4;
+public class Clone2 {
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
+
---
+++ OOP/src/main/java/part4/Clonel.java
@@ -0,0 +1,8 @@
+package part4;
+
+class Clonel implements  Cloneable{
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
